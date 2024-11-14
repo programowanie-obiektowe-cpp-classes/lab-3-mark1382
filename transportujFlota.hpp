afbad17d3a79126f829e6e0130edd3190580a35a
mark1382 <66511007+mark1382@users.noreply.github.com>
2024-11-14 10:09:50 +0100

    Update transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,29 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    {
+    if (towar == 0) {
+        return 0; // Nie tworzymy żadnego statku, gdy towar = 0
+    }
+
+    Stocznia stocznia;
+    unsigned int sumaPrzetransportowanegoTowaru = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    while (sumaPrzetransportowanegoTowaru < towar) {
+        Statek* statek = stocznia(); // Tworzenie losowego statku
+        unsigned int przetransportowanyTowar = statek->transportuj();
+        sumaPrzetransportowanegoTowaru += przetransportowanyTowar;
+
+        // Sprawdzenie, czy stworzony statek był żaglowcem
+        if (dynamic_cast<Zaglowiec*>(statek) != nullptr) {
+            ++liczbaZaglowcow;
+        }
+
+        delete statek; // Usuwanie stworzonego statku
+    }
+
+    return liczbaZaglowcow;
+}
     return 0;
 }
